783ff2be4cf61f90ee82979a8df4e831bf3fc02b
Sebastian Markiton <office@mktnsoft.pl>
2022-04-29 13:46:18 +0200

    Dodanie prototypu formatki ewidencji pacjenta.

---
+++ MediStoma/AppModules/Patient_PF/PacjenciEwidencja_f.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MediStoma.AppModules.Patient_PF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy PacjenciEwidencja_f.xaml
+    /// </summary>
+    public partial class PacjenciEwidencja_f : Window
+    {
+        public PacjenciEwidencja_f()
+        {
+            InitializeComponent();
+        }
+    }
+}
